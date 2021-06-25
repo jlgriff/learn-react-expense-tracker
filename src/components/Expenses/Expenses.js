884eefba6f20a8be03875d5494554ae1b73049d9
John Griffith <john.griffith@dcsg.com>
2021-06-25 13:43:40 -0400

    Added new expense form. Added expense chart.

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import { useState } from "react";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  };
+
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList expenses={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
